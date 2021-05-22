5237cfd736b90dd29061b9a3207b5414cab2cd92
MorozovaO <palomina.1983@gmail.com>
2021-05-22 21:20:52 +0300

    Homework 1

---
+++ src/MainClassTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+    @Test
+    public void testGetLocalNumber() {
+        MainClass mainClass = new MainClass();
+        Assert.assertEquals("Возвращаемое значение getLocalNumber не равно 14!", 14, mainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("Возвращаемое значение getClassNumber не больше 45!", mainClass.getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("Возвращаемое значение getClassString не содержит hello или Hello!", mainClass.getClassString().toLowerCase().contains("hello"));
+    }
+}
